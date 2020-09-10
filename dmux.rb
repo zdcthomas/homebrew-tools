b9ba113f5ebd923e4835841cc11c5d1415e29e78
Zachary Thomas <zdcthomas@yahoo.com>
2020-09-10 00:44:49 -0600

    bumps dmux to v0.6.0

--- dmux.rb
+++ dmux.rb
@@ -1,8 +1,8 @@
 class Dmux < Formula
   desc "Creates and handles tmux workspaces"
   homepage "https://github.com/zdcthomas/dmux"
-  url "https://github.com/zdcthomas/dmux/releases/download/v0.5.2/dmux"
-  sha256 "704678b2719ec650f9fd457cca173edb990935dfa646e33aba0f839cecab721e"
+  url "https://github.com/zdcthomas/dmux/releases/download/v0.6.0/dmux"
+  sha256 "ad5c5c756c2d8d29e15428378051642bb54f21cc72158f2fb73f07c3fa60eff2"
 
   depends_on "fzf"
   depends_on "tmux"
